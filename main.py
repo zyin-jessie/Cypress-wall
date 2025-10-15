0d21a22439e06a3a39913a647b2e003a879aadf6
zyin-jessie <jxieee798@gmail.com>
2025-10-15 09:43:31 +0800

    added gitignore file

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import hashlib
 
-with open ("rockyou.txt", "r") as file:
+with open ("file.txt", "r") as file:
     for line in file:
         password = line.strip()
         hashed = hashlib.md5(password.encode()).hexdigest()
-        if hashed == "482c811da5d5b4bc6d497ffa98491e38":
+        if hashed == "hashed password":
             print(password)
