7ceca50f8dda28ad807d67368c9b90d5532ce5f4
zyin-jessie <jxieee798@gmail.com>
2025-11-13 23:24:52 +0800

    added readme file

---
+++ README.md
@@ -0,0 +1,21 @@
+<div align="center">
+  <a href="./public/toolkit.png" target="_blank">
+    <img src="./public/toolkit.png" alt="PwnStar Toolkit Banner">
+    <br>
+    <img src="https://img.shields.io/badge/Python-3.11.9-%233776AB?style=flat-square&logo=python&logoColor=FFFFFF" alt="python">  
+</a>
+
+# PwnStar-Toolkit
+
+**PwnStar-Toolkit** is a lightweight command-line toolkit designed for **CTF enthusiasts** and security researchers. It helps you quickly **decode classical ciphers**, **crack hashes**, and automate common crypto and reversing tasks.
+
+</div>
+
+---
+## Features
+
+- 🔑 Decode classical ciphers: Vigenère, Caesar, ROT13, etc.  
+- 🛡️ Crack common hash types: MD5, SHA256, and more.
+- ⚡ Fast CLI interface for quick usage  
+- 📂 Easy integration into automated CTF workflows
+
--- src/main.py
+++ src/main.py
@@ -1,9 +1,27 @@
 import hashlib
 
-with open ("../public/wordlist.txt", "r", encoding="latin-1") as file:
+target_hash = "2aa90ee09e690f6f68f529309fda8595323cd83c"
+found = False
+
+print(r"""
+========================================================================
+   ___                 __ _               _____            _ _    _ _   
+  / _ \__      ___ __ / _\ |_ __ _ _ __  /__   \___   ___ | | | _(_) |_ 
+ / /_)/\ \ /\ / / '_ \\ \| __/ _` | '__|   / /\/ _ \ / _ \| | |/ / | __|
+/ ___/  \ V  V /| | | |\ \ || (_| | |     / / | (_) | (_) | |   <| | |_ 
+\/       \_/\_/ |_| |_\__/\__\__,_|_|     \/   \___/ \___/|_|_|\_\_|\__|
+
+========================================================================
+""")
+
+with open("../public/wordlist.txt", "r", encoding="latin-1") as file:
     for line in file:
         password = line.strip()
         hashed = hashlib.sha1(password.encode()).hexdigest()
-        if hashed == "2aa90ee09e690f6f68f529309fda8595323cd83c":
+        if hashed == target_hash:
             print(f"Password: {password}")
+            found = True
             break
+
+if not found:
+    print("0 Password found!")
